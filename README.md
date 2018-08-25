a257f94409dffb4d81b847eb2d6d3739682a3848
freshbrashman <fresh.brash.sarariiman@gmail.com>
2018-08-26 02:08:49 +0900

    nodejsV8で、GCPのBigQueryデータセットを削除するサンプルプログラムを作成。AWSのLambdaからGCPのAPIをキックすることもできた。

---
+++ README.md
@@ -0,0 +1,13 @@
+lambdaに上げるときは、以下の手順を実施
+
+1.動作確認用の以下の行をコメントアウト(たぶん50行目くらい)
+main().catch(console.error)
+
+GCP側でサービスアカウントを作成して、BigQueryのデータセット削除できる権限以上を付与する
+JSONをダウンロードして、allow_bq_storage_user.jsonに上書き保存する
+
+ZIP化する
+$ cd node-gcp-test
+$ zip -r node-gcp-test.zip *
+
+
---
+++ index.js
@@ -0,0 +1,50 @@
+const {google} = require('googleapis');
+const bigquery = google.bigquery('v2');
+const storage = google.storage('v1');
+const dataflow = google.dataflow("v1b3");
+
+//////////////////////　実行準備 ////////////////////////////////////////////////////////////
+// Lambda登録時に環境変数「GOOGLE_APPLICATION_CREDENTIALS」を指定する
+// GOOGLE_APPLICATION_CREDENTIALS=./allow_bq_storage_user.json
+
+// ./allow_bq_storage_user.json に、BigQueryのデータセット削除権限を持ったサービスアカウントの
+// JSONをダウンロードして指定すること
+
+// Lambda実行時に、ハンドラには「index.gcpTest」を指定する
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+async function main () {
+
+  // This method looks for the GCLOUD_PROJECT and GOOGLE_APPLICATION_CREDENTIALS
+  // environment variables.
+  const client = await google.auth.getClient({
+    // Scopes can be specified either as an array or as a single, space-delimited string.
+    scopes: [
+      'https://www.googleapis.com/auth/bigquery',
+      'https://www.googleapis.com/auth/devstorage.full_control'
+    ]
+  });
+
+  // obtain the current project Id
+  const projectId = await google.auth.getDefaultProjectId();
+
+  const request = {
+    projectId,
+    datasetId: 'test_ds_xxx',
+
+    // This is a "request-level" option
+    auth: client
+  };
+
+  const res = await bigquery.datasets.delete(request);
+  console.log(res.data);
+
+  // const res = await compute.zones.list({ project, auth });
+  // console.log(res.data);
+}
+
+// lambdaで動かす場合は、ハンドラに「index.gcpTest」を指定する
+exports.gcpTest = main;
+
+// テスト・デバッグ時に有効化する
+main().catch(console.error);
